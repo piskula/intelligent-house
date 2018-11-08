c932d9031e982c78079e21fbd0d6b7d74b37a15a
Ondrej Oravčok <pista.baci@atlas.sk>
2018-11-08 14:11:49 +0100

    added value parsing

--- raspberry_scripts/connect.py
+++ raspberry_scripts/connect.py
@@ -1,18 +1,24 @@
 import time
-from random import randint
+import re
+import yaml
 import firebase_admin
 from firebase_admin import credentials, db
 
-cred = credentials.Certificate('../service-key.json')
+with open("./config.yml", 'r') as ymlfile:
+  cfg = yaml.load(ymlfile)
+
+cred = credentials.Certificate(cfg['serviceKeyFile'])
 firebase_admin.initialize_app(cred, {
-  'databaseURL': 'https://intelligent-house-test.firebaseio.com/'
+  'databaseURL': cfg['firebaseUrl']
 })
 
 root = db.reference()
 
-for i in range(0, 10):
+while True:
   time.sleep(4)
-  value = randint(20000,25000) / float(1000)
+  file_temp_room_1 = open('w1_slave', 'r').read(90)
+  value = float(re.compile('t=([\d]{3,6})').findall(file_temp_room_1)[0])
+
   root.child('data').child('temp_room_01').push({
-    'value' : value
+    'value': value
   })
---
+++ raspberry_scripts/readme.md
@@ -0,0 +1,4 @@
+### before use:
+
+- `pip3 install firebase_admin`
+- `pip3 install pyyaml`
