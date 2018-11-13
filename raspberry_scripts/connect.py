dcf57c90510638f64bb370ffa8b8ddab3748d194
Ondrej Oravčok <pista.baci@atlas.sk>
2018-11-13 15:50:13 +0100

    resolve killing process by raspberry

--- raspberry_scripts/connect.py
+++ raspberry_scripts/connect.py
@@ -1,5 +1,6 @@
 import yaml
 import os
+from time import sleep
 
 print('starting cron')
 with open("/home/pi/intelligent-house/raspberry_scripts/config.yml", 'r') as ymlfile:
@@ -11,3 +12,4 @@ for temperatureSensor in cfg['sensors']['temperature']:
   pathToFile = cfg['sensors']['temperature'][temperatureSensor]['path']
   # os.system(f'nohup python3 -u temperature.py {pathToFile} {temperatureSensorDelay} {temperatureSensor} &')
   os.system('nohup python3 -u /home/pi/intelligent-house/raspberry_scripts/temperature.py ' + pathToFile + ' ' + str(temperatureSensorDelay) + ' ' + temperatureSensor + ' > /home/pi/out_' + temperatureSensor + '.txt &')
+  sleep(10)
