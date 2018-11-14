e0d3c17ad1d89777eb09daf53018e94d1fa36fad
Ondrej Oravčok <pista.baci@atlas.sk>
2018-11-14 22:44:07 +0100

    REST endpoint for notifications to actual device

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,6 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
+import MessagingPayload = admin.messaging.MessagingPayload;
 
 admin.initializeApp();
 
@@ -11,9 +12,27 @@ exports.fillTimestamp = functions.database.ref('/data/{room_id}/{pushId}').onCre
 
 exports.refreshStatus = functions.https.onRequest((req, res) => {
   admin.database().ref().child("/data/temp_room_2")
-      .orderByChild("timestamp")
-      .limitToLast(1)
-      .once('value')
-      .then(snapshot => res.send(snapshot.val()))
-      .catch(err => res.send("Err: " + err));
+    .orderByChild("timestamp")
+    .limitToLast(1)
+    .once('value')
+    .then(snapshot => res.send(snapshot.val()))
+    .catch(err => res.send("Err: " + err));
+});
+
+exports.sendNotification = functions.https.onRequest((req, res) => {
+  const payload: MessagingPayload = {
+    data: {
+      title: 'Mojko',
+      subtitle: 'Hopko',
+    },
+  };
+
+  admin.database().ref().child("firebaseId")
+    .once('value')
+    .then(snapshot => {
+      admin.messaging().sendToDevice(snapshot.val(), payload)
+        .then(() => res.send("OK"))
+        .catch(err => res.status(500).send("Error occurred while sending notification"));
+    })
+    .catch(err => res.status(500).send("App has not been installed yet (firebaseId not available)"));
 });
